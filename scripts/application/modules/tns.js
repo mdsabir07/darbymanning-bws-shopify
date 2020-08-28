d3c954010fd5c2a0ecab0cf2a0cb957b7c2b0b78
Rob <rob@kbamarketing.co.uk>
2020-08-28 16:06:59 +0100

    Update styles and make slider responsive

--- scripts/application/modules/tns.js
+++ scripts/application/modules/tns.js
@@ -20,7 +20,18 @@ export default function () {
       autoplay: false,
       loop: true,
       controlsContainer: ".slider-4-controls",
-
+      nav: false,
+      responsive: {
+        350: {
+          items: 1,
+        },
+        700: {
+          items: 3,
+        },
+        1000: {
+          items: 4
+        }
+      },
     });
 
   };
