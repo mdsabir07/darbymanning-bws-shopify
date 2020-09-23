db83e8f1547464841ae2cb8097dc5e99f1c6d44d
Terence O'Donoghue <terence@creativelittledots.co.uk>
2020-09-23 11:17:09 +0100

    slider dots

--- scripts/application/modules/tns.js
+++ scripts/application/modules/tns.js
@@ -5,12 +5,31 @@ export default function () {
 	  
 	Array.from(document.querySelectorAll('.js-slider')).forEach(el => {
 		
-		tns({
+		const controls = el.parentNode.querySelector('.js-slider-controls')
+		const dots = controls.querySelector('.js-slider-dots')
+		
+		const slider = tns({
 	      container: el,
 	      items: el.getAttribute('data-items') || 1,
 	      gutter: el.getAttribute('data-gutter') || 0,
 	      slideBy: "page",
-	      controlsContainer: el.parentNode.querySelector(".js-slider-controls"),
+	      controlsContainer: controls,
+	    });
+	    
+	    slider.events.on('transitionEnd', (info) => {
+		    
+		    if(dots) {
+			    
+			    const active = dots.querySelector('.active')
+			    
+			    active && active.classList.remove('active')
+			    
+			    const dot = Array.from(dots.children)[info.displayIndex-1]
+			    
+			    dot && dot.classList.add('active')
+			    
+		    }
+		    
 	    });
 		
 	})
