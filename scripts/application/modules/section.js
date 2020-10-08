670a5a436a55e02cdd2b7a2770d968c4366dc230
darbymanning <darby@kyan.com>
2020-10-08 19:42:54 +0100

    Add linting (#6)
    * Adds Prettier, Stylelint
    
    * Revert linting on config & theme
    
    * Revert theme.liquid

--- scripts/application/modules/section.js
+++ scripts/application/modules/section.js
@@ -1,42 +1,40 @@
-import { register } from "@shopify/theme-sections";
-import * as product from "@shopify/theme-product";
-import {getUrlWithVariant, ProductForm } from "@shopify/theme-product-form";
+import { register } from '@shopify/theme-sections';
+import * as product from '@shopify/theme-product';
+import { getUrlWithVariant, ProductForm } from '@shopify/theme-product-form';
 
 // This function is called whenever the user changes the value of an option input
 function onOptionChange(event) {
-  const variant = event.dataset.variant;
- 
-  if (variant === null) {
-    // The combination of selected options does not have a matching variant
-  } else if (variant && !variant.available) {
-    // The combination of selected options has a matching variant but it is
-    // currently unavailable 
-  } else if (variant && variant.available) {
-    // The combination of selected options has a matching variant and it is
-    // available
-  }
-  if (!variant) return;
-  const url = getUrlWithVariant(window.location.href, variant.id);
-  window.history.replaceState({ path: url }, '', url);
+    const variant = event.dataset.variant;
+
+    if (variant === null) {
+        // The combination of selected options does not have a matching variant
+    } else if (variant && !variant.available) {
+        // The combination of selected options has a matching variant but it is
+        // currently unavailable
+    } else if (variant && variant.available) {
+        // The combination of selected options has a matching variant and it is
+        // available
+    }
+    if (!variant) return;
+    const url = getUrlWithVariant(window.location.href, variant.id);
+    window.history.replaceState({ path: url }, '', url);
 }
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
+    const formElement = document.querySelector('[data-product-form]');
+    const productHandle = formElement
+        ? formElement.dataset.productHandle
+        : null;
 
-	const formElement = document.querySelector('[data-product-form]');
-	const productHandle = formElement ? formElement.dataset.productHandle : null;
-	
-	if(productHandle) {
-			
-		fetch(`/products/${productHandle}.js`)
-		  .then(response => {
-		    return response.json();
-		  })
-		  .then(productJSON => {
-		    const productForm = new ProductForm(formElement, productJSON, {
-		      onOptionChange
-		    });
-		  });
-		  
-	}
-	
-})
+    if (productHandle) {
+        fetch(`/products/${productHandle}.js`)
+            .then((response) => {
+                return response.json();
+            })
+            .then((productJSON) => {
+                const productForm = new ProductForm(formElement, productJSON, {
+                    onOptionChange,
+                });
+            });
+    }
+});
--- scripts/application/modules/tns.js
+++ scripts/application/modules/tns.js
@@ -1,44 +1,36 @@
-import { tns } from "tiny-slider/src/tiny-slider";
+import { tns } from 'tiny-slider/src/tiny-slider';
 export default function () {
+    const init = () => {
+        Array.from(document.querySelectorAll('.js-slider')).forEach((el) => {
+            const controls = el.parentNode.querySelector('.js-slider-controls');
+            const dots = controls.querySelector('.js-slider-dots');
+            const responsive = el.getAttribute('data-responsive');
 
-  const init = () => {
-	  
-	Array.from(document.querySelectorAll('.js-slider')).forEach(el => {
-		
-		const controls = el.parentNode.querySelector('.js-slider-controls')
-		const dots = controls.querySelector('.js-slider-dots')
-		const responsive = el.getAttribute('data-responsive')
-		
-		const slider = tns({
-	      container: el,
-	      items: el.getAttribute('data-items') || 1,
-	      gutter: el.getAttribute('data-gutter') || 0,
-	      slideBy: "page",
-	      controlsContainer: controls,
-	      responsive: responsive ? JSON.parse(responsive) : null
-	    });
-	    
-	    slider.events.on('transitionEnd', (info) => {
-		    
-		    if(dots) {
-			    
-			    const active = dots.querySelector('.active')
-			    
-			    active && active.classList.remove('active')
-			    
-			    const dot = Array.from(dots.children)[info.displayIndex-1]
-			    
-			    dot && dot.classList.add('active')
-			    
-		    }
-		    
-	    });
-		
-  })
-    
+            const slider = tns({
+                container: el,
+                items: el.getAttribute('data-items') || 1,
+                gutter: el.getAttribute('data-gutter') || 0,
+                slideBy: 'page',
+                controlsContainer: controls,
+                responsive: responsive ? JSON.parse(responsive) : null,
+            });
 
-  };
-  const destroy = () => {};
-  const reinit = () => {};
-  return { destroy, reinit, init };
+            slider.events.on('transitionEnd', (info) => {
+                if (dots) {
+                    const active = dots.querySelector('.active');
+
+                    active && active.classList.remove('active');
+
+                    const dot = Array.from(dots.children)[
+                        info.displayIndex - 1
+                    ];
+
+                    dot && dot.classList.add('active');
+                }
+            });
+        });
+    };
+    const destroy = () => {};
+    const reinit = () => {};
+    return { destroy, reinit, init };
 }
