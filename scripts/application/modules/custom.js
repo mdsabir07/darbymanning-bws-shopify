0fca33c780257d889a6876038cf83eb7d967541a
Terence O'Donoghue <terence@creativelittledots.co.uk>
2020-10-19 13:43:25 +0100

    better click handle

--- scripts/application/modules/custom.js
+++ scripts/application/modules/custom.js
@@ -1,8 +1,14 @@
-function toggleNavMenu() {
-  var x = document.getElementById("SiteNav");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
+document.addEventListener('DOMContentLoaded', () => {
+
+	document.getElementById('ToggleMenu').addEventListener('click', () => {
+		
+		let x = document.getElementById("SiteNav");
+		if (x.style.display === "none") {
+			x.style.display = "block";
+		} else {
+			x.style.display = "none";
+		}
+		
+	})	
+	
+})
