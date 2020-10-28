9ddf837c549723f56c2cf1d46f2cb2139d9b8239
unknown <mayur@maven.one>
2020-10-28 18:42:25 +0530

    product detail responsive, blog listing, foot icon remove

--- scripts/application/modules/smooth-scrolling.js
+++ scripts/application/modules/smooth-scrolling.js
@@ -1,13 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
+	var selection = document.querySelector('#footprint') !== null;
 	if(document.querySelector('.product-main__button')) {
 		document.querySelector('.product-main__button').addEventListener('click', function(e) {
 			e.preventDefault();
-			var selection = document.querySelector('#footprint') !== null;
 			if(selection) {
 				document.querySelector('#footprint').scrollIntoView({ behavior: 'smooth' });
 			}
 		});
 	}
 	
-	
+	if(selection) {
+		document.querySelector('.product-main__button').style.display = 'flex';
+	}
+	else {
+		document.querySelector('.product-main__button').remove();
+	}
 });
