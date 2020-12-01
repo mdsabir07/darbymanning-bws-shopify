9e521642cce6eaceb0cc5b7a754df1d6bd816f99
Terence O'Donoghue <terence@creativelittledots.co.uk>
2020-12-01 14:32:06 +0000

    cleanup of metafield work

--- scripts/application/modules/tns.js
+++ scripts/application/modules/tns.js
@@ -8,15 +8,17 @@ document.addEventListener('DOMContentLoaded', () => {
             );
             const dots = controls.querySelector('.js-slider-dots');
             const responsive = container.dataset.responsive;
+            
+            const loop = container.dataset.loop === undefined || Math.abs(container.dataset.loop) ? true : false
 
             const slider = tns({
                 container: container,
                 items: container.dataset.items || 1,
                 gutter: container.dataset.gutter || 0,
-                slideBy: 'page',
                 controlsContainer: controls,
                 responsive: responsive ? JSON.parse(responsive) : null,
                 mouseDrag: true,
+                loop
             });
 
             slider.events.on('transitionEnd', (info) => {
@@ -32,63 +34,41 @@ document.addEventListener('DOMContentLoaded', () => {
                     dot && dot.classList.add('is-active');
                 }
             });
-        }
-    );
-
-    Array.from(document.getElementsByClassName('my-slider')).forEach(
-        (container) => {
-            var count = document.querySelectorAll('.js-slider-controls').length;
             
-            const controls = container.parentNode.querySelector(
-                '.js-slider-controls'
-            );
-            const dots = controls.querySelector('.js-slider-dots');
-            const responsive = container.dataset.responsive;
-
-            if(count == 1) {
-                const slider = tns({
-                    container: container,
-                    items: container.dataset.items || 1,
-                    gutter: container.dataset.gutter || 0,
-                    slideBy: 'page',
-                    loop: false,
-                    controls:false,
-                    responsive: responsive ? JSON.parse(responsive) : null,
-                    mouseDrag: true,
-                });
-                var divsToHide =document.querySelectorAll('.js-slider-controls');
-
-                for(var i = 0; i < divsToHide.length; i++){
-                    divsToHide[i].style.visibility = "hidden"; // or
-                    divsToHide[i].style.display = "none"; // depending on what you're doing
-                }
-
-            } else {
-                const slider = tns({
-                    container: container,
-                    items: container.dataset.items || 1,
-                    gutter: container.dataset.gutter || 0,
-                    slideBy: 'page',
-                    loop: false,
-                    controlsContainer: controls,
-                    responsive: responsive ? JSON.parse(responsive) : null,
-                    mouseDrag: true,
-                });
-
-                slider.events.on('transitionEnd', (info) => {
-                    if (dots) {
-                        const active = dots.querySelector('.is-active');
-
-                        active && active.classList.remove('is-active');
-
-                        const dot = Array.from(dots.children)[
-                            info.displayIndex - 1
-                        ];
-
-                        dot && dot.classList.add('is-active');
-                    }
-                });
-            }
+            if(dots) {
+	            
+	            dots.addEventListener('click', e => e.preventDefault())
+            
+	            Array.from(dots.children).forEach(el => el.addEventListener('click', (e) => {
+		            
+		            e.preventDefault()
+		            
+		            const index = parseInt(e.currentTarget.dataset.goto)-1
+		            
+		            slider.goTo(index)
+		            
+				}))
+			
+			}
+            
+            if(!loop) {
+            
+	            controls.querySelector('[data-controls="next"]').addEventListener('click', (e) => {
+		            
+		            const info = slider.getInfo()
+		            
+		            if(info.displayIndex === info.slideCount) {
+			            
+			            e.preventDefault()
+			            
+			            slider.goTo(0)
+			            
+		            }
+		            
+	            })
+	            
+			}
+            
         }
     );
 
