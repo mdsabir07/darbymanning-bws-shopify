3e75ac9e7fea4e2635df446091adbff40e3b4c0c
Mayur Sutariya <sutariyamayur@gmail.com>
2020-12-01 17:35:49 +0530

    resolve bugs 6,9,11, M:7

--- scripts/application/modules/tns.js
+++ scripts/application/modules/tns.js
@@ -34,4 +34,62 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         }
     );
+
+    Array.from(document.getElementsByClassName('my-slider')).forEach(
+        (container) => {
+            var count = document.querySelectorAll('.js-slider-controls').length;
+            
+            const controls = container.parentNode.querySelector(
+                '.js-slider-controls'
+            );
+            const dots = controls.querySelector('.js-slider-dots');
+            const responsive = container.dataset.responsive;
+
+            if(count == 1) {
+                const slider = tns({
+                    container: container,
+                    items: container.dataset.items || 1,
+                    gutter: container.dataset.gutter || 0,
+                    slideBy: 'page',
+                    loop: false,
+                    controls:false,
+                    responsive: responsive ? JSON.parse(responsive) : null,
+                    mouseDrag: true,
+                });
+                var divsToHide =document.querySelectorAll('.js-slider-controls');
+
+                for(var i = 0; i < divsToHide.length; i++){
+                    divsToHide[i].style.visibility = "hidden"; // or
+                    divsToHide[i].style.display = "none"; // depending on what you're doing
+                }
+
+            } else {
+                const slider = tns({
+                    container: container,
+                    items: container.dataset.items || 1,
+                    gutter: container.dataset.gutter || 0,
+                    slideBy: 'page',
+                    loop: false,
+                    controlsContainer: controls,
+                    responsive: responsive ? JSON.parse(responsive) : null,
+                    mouseDrag: true,
+                });
+
+                slider.events.on('transitionEnd', (info) => {
+                    if (dots) {
+                        const active = dots.querySelector('.is-active');
+
+                        active && active.classList.remove('is-active');
+
+                        const dot = Array.from(dots.children)[
+                            info.displayIndex - 1
+                        ];
+
+                        dot && dot.classList.add('is-active');
+                    }
+                });
+            }
+        }
+    );
+
 });
